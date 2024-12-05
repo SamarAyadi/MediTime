54e08c6f71f27e5096c48b3d6b055f51f89425e2
Samar Ayadi <74440603+SamarAyadi@users.noreply.github.com>
2024-12-05 07:14:43 +0100

    add  Appointment Page

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -93,7 +93,7 @@ export const doctors = [
         speciality: 'General physician',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Ben Youssef has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Ben Youssef has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Ben Youssef has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
         fees: 50,
         address: {
             line1: '17th Cross, Richmond',
